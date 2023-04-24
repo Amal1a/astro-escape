3d93e13807033048b43eea75b27f045c0ac280b7
Freja Fischer <frej657e@stud.kea.dk>
2023-04-24 17:10:26 +0200

    Final for now

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -2,6 +2,7 @@ const carousel = document.querySelector(".carousel");
 const slides = carousel.querySelectorAll(".slide");
 const prevButton = carousel.querySelector(".prev-button");
 const nextButton = carousel.querySelector(".next-button");
+const accepted = document.querySelector(".accepted");
 
 let currentSlide = 0;
 
@@ -9,10 +10,20 @@ function showSlide(n) {
   slides[currentSlide].classList.remove("active");
   currentSlide = (n + slides.length) % slides.length;
   slides[currentSlide].classList.add("active");
+
+  //   if (document.querySelector(".slide.active").accepted !== true) {
+  //     console.log("I am false");
+  //     // slides[currentSlide].classList.add("active");
+  //   }
+  //   if (document.querySelector(".slide.active").accepted === true) {
+  //     console.log("I am true");
+  //     // slides[currentSlide].classList.add("hide");
+  //   }
 }
 
 function showNextSlide() {
   showSlide(currentSlide + 1);
+  //   console.log("I am" + currentSlide);
 }
 
 function showPrevSlide() {
